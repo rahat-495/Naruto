cccc82ffde46b478ae613aeee8f143d4a70caa3c
rahat-495 <md8702687@gmail.com>
2025-06-12 18:24:28 +0600

    smooth scrolling are implemented use lenis !

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/shared/Navbar";
+import {ReactLenis} from "@/app/utils/global/lenis"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,10 +27,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-theme="black">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Navbar />
-        {children}
-      </body>
+      
+      <ReactLenis root>
+
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased scroll-smooth`}>
+          
+          <Navbar />
+          {children}
+
+        </body>
+      </ReactLenis>
+
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
 
+"use client" ;
+import { useEffect } from "react";
 import Banner from "./components/sections/home/Banner";
 import Childhood from "./components/sections/home/Childhood";
+import Lenis from "lenis";
 
 export default function Home() {
+
+  useEffect(() => {
+    const lenis = new Lenis({duration : 1.5}) ;
+    const raf = (time : any) => {
+      lenis.raf(time) ;
+      requestAnimationFrame(raf) ;
+    }
+    requestAnimationFrame(raf) ;
+  } , [])
+
   return (
     <div className="min-h-screen overflow-x-clip font-[family-name:var(--font-geist-sans)]">
       <Banner />

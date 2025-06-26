edf9bbec243df9b43c6bf048844524e796dbdc01
rahat-495 <md8702687@gmail.com>
2025-06-27 04:56:21 +0600

    section 6 on working !

--- app/components/sections/home/NarutosDialogue.tsx
+++ app/components/sections/home/NarutosDialogue.tsx
@@ -1,8 +1,10 @@
 
 const NarutosDialogue = () => {
     return (
-        <div className="h-screen bg-black">
+        <div className="h-screen bg-black flex flex-col justify-center">
             
+            <p className="text-[600px] whitespace-nowrap">Naruto Uzumaki — I'm not gonna run away, I never go back on my word ! That's my ninja way !</p>
+
         </div>
     );
 };
